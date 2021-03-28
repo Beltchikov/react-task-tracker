90d772fc49a46d3bf25b3a0bcc76e5b3faa72175
Jogger <beltchikov@gmx.de>
2021-03-28 13:11:56 +0200

    improvements

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import {useState} from 'react'
+import { useState } from 'react'
 
 function App() {
 
   const [tasks, setTasks] = useState([
     {
-        id:1,
-        text: 'Doctor Appointment',
-        day: '05.02 02:30',
-        reminder: true
+      id: 1,
+      text: 'Doctor Appointment',
+      day: '05.02 02:30',
+      reminder: true
     },
     {
-        id:2,
-        text: 'Meeting an School',
-        day: '06.02 01:30',
-        reminder: true
+      id: 2,
+      text: 'Meeting an School',
+      day: '06.02 01:30',
+      reminder: true
     },
     {
-        id:3,
-        text: 'Food Shopping',
-        day: '07.02 02:30',
-        reminder: true
+      id: 3,
+      text: 'Food Shopping',
+      day: '07.02 02:30',
+      reminder: true
     }
-])
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id
+    ? {...task, reminder: !task.reminder}
+    : task))
+  }
 
   return (
     <div className="container">
       <a href='https://www.youtube.com/watch?v=w7ejDZ8SWv8'>https://www.youtube.com/watch?v=w7ejDZ8SWv8</a>
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0
+        ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+        : 'No Tasks to Show'}
+
     </div>
   );
 }
